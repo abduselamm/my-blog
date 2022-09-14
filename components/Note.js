dff2d79a5e0130265eb79abfd26a551d33639667
Humed-Muhammad <abc45171@gmail.com>
2022-09-14 21:56:12 +0300

    🚑 HOTFIX: 🆕 NEW:  email

--- components/Note.js
+++ components/Note.js
@@ -1,5 +1,17 @@
 import { Container } from './Container'
 
-export const Note = ({ children, className }) => {
-  return <Container className={`bg-cyan-300 text-gray-700 ${className}`}>{children}</Container>
+export const Note = ({ children, className, type = 'info' }) => {
+  const design = {
+    info: 'bg-teal-100',
+    warning: 'bg-yellow-200',
+    error: 'bg-red-200',
+  }
+
+  return (
+    <div
+      className={` flex-col rounded-md ${design[type]} bo border-l-8  p-4 px-5 text-slate-600 ${className}`}
+    >
+      {children}
+    </div>
+  )
 }
