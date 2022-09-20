2fe948a9a4323d0e6fba6c3e729087f5849fa91e
Humed-Muhammad <abc45171@gmail.com>
2022-09-20 22:22:59 +0300

    🔥 REMOVE:  GA

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -1,7 +1,7 @@
-import GA from './GoogleAnalytics'
+// import GA from './GoogleAnalytics'
 import Plausible from './Plausible'
-import SimpleAnalytics from './SimpleAnalytics'
-import Umami from './Umami'
+// import SimpleAnalytics from './SimpleAnalytics'
+// import Umami from './Umami'
 import siteMetadata from '@/data/siteMetadata'
 
 const isProduction = process.env.NODE_ENV === 'production'
@@ -13,7 +13,6 @@ const Analytics = () => {
       {/* {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
       {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
       {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />} */}
-      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
     </>
   )
 }
