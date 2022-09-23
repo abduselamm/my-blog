b04c6c79be808a5ff39728248796908f0a945d5a
Humed-Muhammad <abc45171@gmail.com>
2022-09-23 19:40:19 +0300

    💄 UI:  ads

--- components/ads/index.js
+++ components/ads/index.js
@@ -3,24 +3,9 @@ import { GoogleAdsScript, GoogleUnitAdsComponent } from './googleUnitAds'
 export const GoogleScript = () => {
   // const isProduction = process.env.NODE_ENV === 'production'
   return (
-    <>
+    <div className="h-auto w-full">
       <GoogleAdsScript />
       <GoogleUnitAdsComponent />
-    </>
+    </div>
   )
-}
-
-{
-  /* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3652123808889795"
-     crossorigin="anonymous"></script>
-<!-- display ads -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3652123808889795"
-     data-ad-slot="2637034645"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script> */
 }
