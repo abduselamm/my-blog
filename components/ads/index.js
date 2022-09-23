26bf7bac80c0afa246baebfe7485f41d2dff5032
Humed-Muhammad <abc45171@gmail.com>
2022-09-23 17:33:20 +0300

    ⚡ PERFORMANCE:

--- components/ads/index.js
+++ components/ads/index.js
@@ -3,9 +3,24 @@ import { GoogleAdsScript, GoogleUnitAdsComponent } from './googleUnitAds'
 export const GoogleScript = () => {
   // const isProduction = process.env.NODE_ENV === 'production'
   return (
-    <div>
+    <>
       <GoogleAdsScript />
       <GoogleUnitAdsComponent />
-    </div>
+    </>
   )
+}
+
+{
+  /* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3652123808889795"
+     crossorigin="anonymous"></script>
+<!-- display ads -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3652123808889795"
+     data-ad-slot="2637034645"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script> */
 }
