55d0095a62b945f30a3303631d2080edce014fe6
Humed-Muhammad <abc45171@gmail.com>
2022-10-01 15:27:18 +0300

    🚑 HOTFIX:

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -4,7 +4,7 @@ export default function SectionContainer({ children, showads = true }) {
   return (
     <div className="flex">
       {showads ? (
-        <div className="relative hidden w-72 flex-col overflow-auto bg-red-300 p-4 sm:flex">
+        <div className="relative hidden w-72 flex-col overflow-auto p-4 sm:flex">
           <GoogleVerticalAdUnit />
           <GoogleVerticalAdUnit />
           <GoogleVerticalAdUnit />
@@ -14,7 +14,7 @@ export default function SectionContainer({ children, showads = true }) {
       ) : null}
       <div className="mx-auto  max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
       {showads ? (
-        <div className="relative hidden w-72 flex-col overflow-auto bg-red-300 p-4 sm:flex">
+        <div className="relative hidden w-72 flex-col overflow-auto p-4 sm:flex">
           <GoogleVerticalAdUnit />
           <GoogleVerticalAdUnit />
           <GoogleVerticalAdUnit />
