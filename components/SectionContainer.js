7d6bcf992a8b2cff7532a98fffd348b910cdcba4
Humed-Muhammad <abc45171@gmail.com>
2022-10-01 16:19:31 +0300

    🚑 HOTFIX: 🎨 IMPROVE:

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -2,7 +2,7 @@ import { GoogleVerticalAdUnit } from './ads'
 
 export default function SectionContainer({ children, showads = true }) {
   return (
-    <>
+    <div className="flex h-auto">
       {showads ? (
         <div className="relative hidden w-72 flex-col overflow-auto py-3 px-5 sm:flex">
           <GoogleVerticalAdUnit path="1" />
@@ -10,7 +10,7 @@ export default function SectionContainer({ children, showads = true }) {
           <GoogleVerticalAdUnit path="3" />
         </div>
       ) : null}
-      <div className="mx-auto  max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+      <div className="mx-auto  max-w-full px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
       {showads ? (
         <div className="relative hidden w-72 flex-col overflow-auto p-4 px-5 sm:flex">
           <GoogleVerticalAdUnit path="5" />
@@ -18,6 +18,6 @@ export default function SectionContainer({ children, showads = true }) {
           <GoogleVerticalAdUnit path="7" />
         </div>
       ) : null}
-    </>
+    </div>
   )
 }
