c6a771d1d8c01fc48f7f4e153a7a89c95759fd5c
Humed-Muhammad <abc45171@gmail.com>
2022-10-01 16:30:04 +0300

    💄 UI:  fixingggg

--- components/ads/index.js
+++ components/ads/index.js
@@ -3,7 +3,7 @@ import { GoogleHorizontalUnitAdsComponent, GoogleVerticalUnitAdsComponent } from
 export const GoogleHorizontalAdUnit = ({ path }) => {
   const isProduction = process.env.NODE_ENV === 'production'
   return (
-    <div className="h-auto w-full overflow-auto">
+    <div className="h-auto w-full">
       {isProduction ? <GoogleHorizontalUnitAdsComponent path={path} /> : null}
     </div>
   )
@@ -12,7 +12,7 @@ export const GoogleHorizontalAdUnit = ({ path }) => {
 export const GoogleVerticalAdUnit = ({ path }) => {
   const isProduction = process.env.NODE_ENV === 'production'
   return (
-    <div className="h-auto w-full overflow-auto">
+    <div className="h-auto w-full">
       {isProduction ? <GoogleVerticalUnitAdsComponent path={path} /> : null}
     </div>
   )
