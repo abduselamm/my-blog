32fa214d3a5ff31e4a7762c1c705b60451e414b3
Humed-Muhammad <abc45171@gmail.com>
2022-10-01 15:09:32 +0300

    🚑 HOTFIX:

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -3,14 +3,14 @@ import { GoogleVerticalAdUnit } from './ads'
 export default function SectionContainer({ children }) {
   return (
     <div className="flex">
-      <div className="hidden flex-grow flex-col overflow-auto p-4 sm:flex">
+      <div className="relative hidden flex-grow flex-col overflow-auto p-4 sm:flex">
         <GoogleVerticalAdUnit path="1" />
         <GoogleVerticalAdUnit path="2" />
         <GoogleVerticalAdUnit path="3" />
         <GoogleVerticalAdUnit path="4" />
       </div>
       <div className="mx-auto  max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
-      <div className="hidden flex-grow flex-col overflow-auto p-4 sm:flex">
+      <div className="relative hidden flex-grow flex-col overflow-auto p-4 sm:flex">
         <GoogleVerticalAdUnit path="5" />
         <GoogleVerticalAdUnit path="6" />
         <GoogleVerticalAdUnit path="7" />
