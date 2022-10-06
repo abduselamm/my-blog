d50016e7f531dc430e081e2e27a904dd0a2de0fb
Humed-Essie <abc45171@gmail.com>
2022-10-06 16:35:54 +0300

    Update SectionContainer.js

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -4,14 +4,14 @@ export default function SectionContainer({ children, showads = true }) {
   return (
     <div className="flex h-auto">
       {showads ? (
-        <div className="relative hidden w-72 flex-col justify-end overflow-auto py-3 px-5 sm:flex h-screen">
+        <div className="h-screen w-72 flex-col justify-end overflow-auto py-3 px-5 sm:flex">
           <GoogleVerticalAdUnit path="1" />
 
         </div>
       ) : null}
       <div className="mx-auto  max-w-full px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
       {showads ? (
-        <div className="relative hidden w-72 flex-col overflow-auto p-4 px-5 sm:flex h-screen ">
+        <div className="h-screen w-72 flex-col justify-end overflow-auto py-3 px-5 sm:flex">
           <GoogleVerticalAdUnit path="3" />
         </div>
       ) : null}
