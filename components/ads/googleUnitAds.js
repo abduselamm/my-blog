e2009df93943c232fecad2ef04601bfb0527d117
Humed-Muhammad <abc45171@gmail.com>
2022-10-08 13:44:23 +0300

    🔥 REMOVE:  unused code

--- components/ads/googleUnitAds.js
+++ components/ads/googleUnitAds.js
@@ -25,27 +25,27 @@ export const GoogleHorizontalUnitAdsComponent = ({ path }) => {
   )
 }
 
-export const GoogleVerticalUnitAdsComponent = ({ path }) => {
-  useEffect(() => {
-    try {
-      window.adsbygoogle = window.adsbygoogle || []
-      window.adsbygoogle.push({})
-    } catch (error) {
-      console.log(error)
-    }
-  }, [path])
-  return (
-    <div className="my-2 h-auto w-full">
-      <ins
-        className="adsbygoogle"
-        style={{
-          display: 'block',
-        }}
-        data-ad-client="ca-pub-3652123808889795"
-        data-ad-slot="4825962131"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </div>
-  )
-}
+// export const GoogleVerticalUnitAdsComponent = ({ path }) => {
+//   useEffect(() => {
+//     try {
+//       window.adsbygoogle = window.adsbygoogle || []
+//       window.adsbygoogle.push({})
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }, [path])
+//   return (
+//     <div className="my-2 h-auto w-full">
+//       <ins
+//         className="adsbygoogle"
+//         style={{
+//           display: 'block',
+//         }}
+//         data-ad-client="ca-pub-3652123808889795"
+//         data-ad-slot="4825962131"
+//         data-ad-format="auto"
+//         data-full-width-responsive="true"
+//       ></ins>
+//     </div>
+//   )
+// }
