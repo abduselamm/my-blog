9fd48251e01e41f3c8fdbcae97064071945061f7
Humed-Muhammad <abc45171@gmail.com>
2022-10-10 21:27:16 +0300

    🎨 IMPROVE:

--- components/LiveShow.js
+++ components/LiveShow.js
@@ -27,3 +27,24 @@ export const LiveShow = ({ Code, showCode = true, title = 'Live code' }) => {
     </LiveProvider>
   )
 }
+
+export const LiveShowString = ({ Code, showCode = true, title = 'Live code' }) => {
+  return (
+    <LiveProvider code={Code}>
+      <h2>{title}</h2>
+      <div className="mb-5 flex h-auto w-full flex-wrap justify-around divide-x-2 overflow-auto rounded-r-md rounded-l-none bg-gray-100 ">
+        {showCode && (
+          <div className="flex h-auto w-1/2 items-center justify-center overflow-auto rounded-l-md bg-gray-900">
+            <LiveEditor />
+          </div>
+        )}
+        <div
+          className={`flex h-auto ${showCode ? 'w-1/2' : 'w-full'}  items-center justify-center`}
+        >
+          <LivePreview />
+        </div>
+      </div>
+      <LiveError className="bg-red-600" />
+    </LiveProvider>
+  )
+}
--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -6,7 +6,6 @@ import { useState } from 'react'
 import Pagination from '@/components/Pagination'
 import formatDate from '@/lib/utils/formatDate'
 import Image from 'next/image'
-import { GoogleHorizontalAdUnit } from '@/components/ads'
 
 export default function ListLayout({ posts, title, initialDisplayPosts = [], pagination }) {
   const [searchValue, setSearchValue] = useState('')
@@ -22,7 +21,6 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <GoogleHorizontalAdUnit path="List layout" />
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900  dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
