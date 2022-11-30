0f85e013b94cae018c506d6808b1745e57b54df7
Humed-Muhammad <abc45171@gmail.com>
2022-11-30 12:19:56 +0300

    🔧 CONFIG: Google analytics

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -3,16 +3,17 @@ import Plausible from './Plausible'
 // import SimpleAnalytics from './SimpleAnalytics'
 // import Umami from './Umami'
 import siteMetadata from '@/data/siteMetadata'
+import GAScript from './GoogleAnalytics'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 const Analytics = () => {
   return (
     <>
-      {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}
+      {/* {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />} */}
       {/* {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
-      {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
-      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />} */}
+      {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />} */}
+      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GAScript />}
     </>
   )
 }
